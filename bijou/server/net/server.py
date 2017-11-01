8f8e29d591aa895e5dbf17df1c9f26585b726ee6
murt <murt@unpossible.io>
2017-11-01 15:01:20 +1000

    #3: Switching to aiohttp from Twisted

--- bijou/server/net/server.py
+++ bijou/server/net/server.py
@@ -1,8 +1,8 @@
-
+from aiohttp import web
 
 class Server:
     """
-    HTTP(S) endpoint for GraphQL queries
+    HTTP(S) endpoint for GraphQL queries and static content
     """
 
     def __init__(self):
